01368565126c88a6114d1e82fa4784d2961a014f
fudongbai <296179868@qq.com>
2021-02-22 20:45:12 +0800

    using fixed sol version

--- contracts/interfaces/IProxyInitialize.sol
+++ contracts/interfaces/IProxyInitialize.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity 0.6.4;
 
 interface IProxyInitialize {
     function initialize(string calldata name, string calldata symbol, uint8 decimals, uint256 amount, bool mintable, address owner) external;
